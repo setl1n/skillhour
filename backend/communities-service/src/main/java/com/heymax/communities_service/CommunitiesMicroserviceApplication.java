5bd6f207e4c49425eb12a2b2fa90620eb82c577c
setl1n <kaungsetlin2008@gmail.com>
2025-01-18 13:03:50 +0800

    chore (backend): init communities microservice

---
+++ backend/communities-service/src/main/java/com/heymax/communities_service/CommunitiesMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.heymax.communities_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunitiesMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunitiesMicroserviceApplication.class, args);
+	}
+
+}
---
+++ backend/communities-service/src/test/java/com/heymax/communities_service/CommunitiesMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heymax.communities_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunitiesMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
